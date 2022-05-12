4ce4ee5bd264a080270f09cdd161f80abbb33951
Philip Lages <psilvaelages@gmail.com>
2022-05-12 18:37:16 -0300

    atualiza exercícios de operadores logicos

--- Fundamentos do Desenvolvimento Web/Bloco 4: Introdução - JavaScript/4.1: JavaScript - Primeiros passos/OperadoresLogicos.js
+++ Fundamentos do Desenvolvimento Web/Bloco 4: Introdução - JavaScript/4.1: JavaScript - Primeiros passos/OperadoresLogicos.js
@@ -12,3 +12,11 @@ if (currentHour >= 22) {
   message = "Hmmm, cheiro de café recém passado";
 }
 console.log(message);
+
+let weekDay = "quarta-feita";
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+} else {
+  console.log("FINALMENTE, descanso merecido UwU");
+}
+
