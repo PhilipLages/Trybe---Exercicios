6a832111142f84dd1eaac5618533105d86ef0f1b
Philip Lages <psilvaelages@gmail.com>
2022-05-16 19:58:16 -0300

    Adiciona exercício 3

--- Fundamentos do Desenvolvimento Web/Bloco 4: Introdução - JavaScript/4.3: JavaScript - Lógica de Programação e Algoritmos/exercicios.js
+++ Fundamentos do Desenvolvimento Web/Bloco 4: Introdução - JavaScript/4.3: JavaScript - Lógica de Programação e Algoritmos/exercicios.js
@@ -1,2 +1,25 @@
-// O fatorial é representado pelo sinal !
-// ! = 4 x 3 x 2 x 1 = 24
+// 1:
+let fatorial = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let valor = 10;
+let resultado = valor;
+
+for (let index = 0; index < fatorial.length; index += 1) {
+  resultado *= fatorial[index] ;   
+}
+console.log(resultado);
+
+// 2:
+let word = 'tryber';
+let backWord = '';
+
+for (let index = word.length - 1; index >= 0; index -= 1) {
+    backWord += word[index];    
+}
+console.log(backWord);
+
+// 3:
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+
+
+
