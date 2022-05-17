b993897a68676c1503b1187feb4f5d42a6019c59
Philip Lages <psilvaelages@gmail.com>
2022-05-17 15:34:03 -0300

    Adiciona exercicio 4

--- Fundamentos do Desenvolvimento Web/Bloco 4: Introdução - JavaScript/4.3: JavaScript - Lógica de Programação e Algoritmos/exercicios.js
+++ Fundamentos do Desenvolvimento Web/Bloco 4: Introdução - JavaScript/4.3: JavaScript - Lógica de Programação e Algoritmos/exercicios.js
@@ -28,7 +28,7 @@ for (let index = 0; index < array.length; index += 1) {
   }
   console.log(biggestWord);
 
-array = ['java', 'javascript', 'python', 'html', 'css', 'ab'];
+array = ['java', 'javascript', 'python', 'html', 'css'];
 let smallestWord = array[0];
 
 for (let index = 0; index < array.length; index += 1) {  
@@ -38,8 +38,26 @@ for (let index = 0; index < array.length; index += 1) {
   }
   console.log(smallestWord);
 
-  // 4:
+  // 4:   
+  let maiorNumeroPrimo = 0;
   
+  for (let dividendo = 2; dividendo < 100; dividendo += 1) {    
+    let divisores = 0;   
+    for (let divisor = 2; divisor < dividendo; divisor += 1) {        
+      if (dividendo % divisor === 0) {
+        divisores += 1;
+      }     
+    }
+    if (divisores === 0 && dividendo > maiorNumeroPrimo) {
+      maiorNumeroPrimo = dividendo;
+      
+    }
+  }
+  console.log(maiorNumeroPrimo);
+    
+ 
+  
+
 
 
 
