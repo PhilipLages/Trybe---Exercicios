596b56eeb8851cf28ca3f9e8140caad5d85fae72
Philip Lages <psilvaelages@gmail.com>
2022-05-17 22:57:46 -0300

    Termina exercicios de Objetos

--- Fundamentos do Desenvolvimento Web/Bloco 4: Introdução - JavaScript/4.4: JavaScript - Objetos e funções/exercicios.js
+++ Fundamentos do Desenvolvimento Web/Bloco 4: Introdução - JavaScript/4.4: JavaScript - Objetos e funções/exercicios.js
@@ -1,3 +1,5 @@
+// Parte 1 - Objetos e For/In:
+
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
@@ -22,4 +24,50 @@ for (let key in info) {
 };
 
 // 5:
+info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim',
+};
 
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (let key in info) {  
+  
+};
+
+// 6:
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+// let livroFavorito = leitor.livrosFavoritos[0];
+console.log('O livro favorito de ' + ' ' + leitor['nome'] + ' ' + leitor['sobrenome'] + 'se chama' + leitor.livrosFavoritos[0]['titulo']);
+
+// 7:
+leitor.livrosFavoritos.push ( {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  },
+);
+// console.log(leitor['livrosFavoritos']);
+
+// 8:
+console.log("Julia tem " + leitor.livrosFavoritos.length + " livros favoritos");
+
+// Parte 2 - Funções:
