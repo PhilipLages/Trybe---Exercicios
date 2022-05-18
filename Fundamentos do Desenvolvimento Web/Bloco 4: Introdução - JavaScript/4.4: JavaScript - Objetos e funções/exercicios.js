5e09303213c3e5b2ec88cac3d5fd9c654cb8d281
Philip Lages <psilvaelages@gmail.com>
2022-05-18 13:58:26 -0300

    Adiciona exercicio 4 da parte 2

--- Fundamentos do Desenvolvimento Web/Bloco 4: Introdução - JavaScript/4.4: JavaScript - Objetos e funções/exercicios.js
+++ Fundamentos do Desenvolvimento Web/Bloco 4: Introdução - JavaScript/4.4: JavaScript - Objetos e funções/exercicios.js
@@ -56,7 +56,7 @@ let leitor = {
   ],
 };
 // let livroFavorito = leitor.livrosFavoritos[0];
-console.log('O livro favorito de ' + ' ' + leitor['nome'] + ' ' + leitor['sobrenome'] + 'se chama' + leitor.livrosFavoritos[0]['titulo']);
+// console.log('O livro favorito de ' + ' ' + leitor['nome'] + ' ' + leitor['sobrenome'] + 'se chama' + leitor.livrosFavoritos[0]['titulo']);
 
 // 7:
 leitor.livrosFavoritos.push ( {
@@ -71,3 +71,63 @@ leitor.livrosFavoritos.push ( {
 console.log("Julia tem " + leitor.livrosFavoritos.length + " livros favoritos");
 
 // Parte 2 - Funções:
+
+// 1:
+function verificaPalindromo(palavra) {
+  let palindromo = '';
+  for (let index = palavra.length - 1; index >= 0; index -= 1) {
+    palindromo += palavra[index];      
+  }
+  if (palavra === palindromo) {    
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(verificaPalindromo('carro'));
+
+// 2:
+function encontraIndiceMaiorValor(array) {  
+  let indiceMaiorValor = 0;
+  for (let index in array) {    
+    if (array[index] > array[indiceMaiorValor]) {
+      indiceMaiorValor = index;
+    }        
+  }
+  return indiceMaiorValor;
+}
+console.log(encontraIndiceMaiorValor([2, 3, 6, 7, 10, 1]));
+
+// 3:
+function encontraIndiceMenorValor(array) {  
+  let indiceMenorValor = 0;
+  for (let index in array) {    
+    if (array[index] < array[indiceMenorValor]) {
+      indiceMenorValor = index;
+    }        
+  }
+  return indiceMenorValor;
+}
+console.log(encontraIndiceMenorValor([2, 4, 6, 7, 10, 0, -3]));
+
+// 4:
+function AchaMaiorNome(nomes) {
+  let maiorNome = nomes[0];
+  for (let index in nomes) {
+    if (nomes[index].length > maiorNome.length) {
+      maiorNome = nomes[index];
+    }    
+  }
+  return maiorNome;  
+}
+console.log(AchaMaiorNome(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+
+// 5:
+function achaRepeticao(numbers) {
+  let maisRepete = numbers[0];
+  for (let number in numbers) {
+      
+  }
+}
+  
+
