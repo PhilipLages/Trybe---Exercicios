d923b277fc0880c8e2c44a5e121cfd2c953795cd
Philip Lages <psilvaelages@gmail.com>
2022-05-20 13:11:16 -0300

    Adiciona exercicios resolvidos do dia 5.1

--- Fundamentos do Desenvolvimento Web/Bloco 05: JavaScript: DOM, Eventos e Web Storage/5.1: JavaScript: DOM e Seletores/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 05: JavaScript: DOM, Eventos e Web Storage/5.1: JavaScript: DOM e Seletores/script.js
@@ -5,16 +5,39 @@ function textChange () {
 }
 textChange();
 
-// // 2:
-// function changeYellowToGreen () {
-//   let changeColor1 = document.getElementsByClassName('main-content');
-//   changeColor1[0].style.backgroundColor = 'rgb(76,164,109)';
-// }
-// changeYellowToGreen();
+// 2:
+function changeYellowToGreen () {
+  let changeColor1 = document.getElementsByClassName('main-content');
+  changeColor1[0].style.backgroundColor = 'rgb(76,164,109)';
+}
+changeYellowToGreen();
 
-//   // 3:
-//   function changeRedToWhite () {
-//     let changeColor2 = document.getElementsByClassName('center-content');
-//     changeColor2[0].style.backgroundColor = 'white';
-//   }
-//   changeRedToWhite();
+// 3:
+  function changeRedToWhite () {
+    let changeColor2 = document.getElementsByClassName('center-content');
+    changeColor2[0].style.backgroundColor = 'white';
+  }
+  changeRedToWhite();
+
+  // 4:
+  function changeH1 () {
+    let title = document.getElementsByClassName('title');
+    title[0].innerHTML = 'Exercício 5.1 - JavaScript: DOM e Seletores';
+  }
+  changeH1();
+
+  // 5:
+function upperCase () {
+  let changeCase = document.getElementsByTagName('p');
+  changeCase[0].style.textTransform = 'upperCase';
+}
+upperCase();
+
+// 6:
+function showTags () {
+  let tags = document.getElementsByTagName('p');
+  for (index = 0; index < tags.length; index += 1) {
+    console.log(tags[index].innerHTML);
+  }  
+};
+showTags();
