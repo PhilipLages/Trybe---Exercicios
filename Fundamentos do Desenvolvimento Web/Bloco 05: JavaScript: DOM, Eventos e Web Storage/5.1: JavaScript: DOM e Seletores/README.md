35dbbfd95f914619c9e31b86245dfcb4076ba475
Philip Lages <psilvaelages@gmail.com>
2022-05-20 12:10:17 -0300

    Organiza arquivos em seus respectivos repositótios

---
+++ Fundamentos do Desenvolvimento Web/Bloco 05: JavaScript: DOM, Eventos e Web Storage/5.1: JavaScript: DOM e Seletores/README.md
@@ -0,0 +1,7 @@
+É hora de aplicar seus conhecimentos em manipular os elementos do HTML!
+A seguir, você verá um trecho de código HTML e CSS.
+Crie um arquivo HTML dentro do diretório exercises/javascript-dom-eventos-e-web-storage_1 e copie o código a seguir.
+Leia as instruções que estão dentro de um comentário na tag <script>.
+Não se esqueça de fazer um commit a cada exercício!
+
+Os exercícios se encontram dentro de index.html.
--- Fundamentos do Desenvolvimento Web/Bloco 05: JavaScript: DOM, Eventos e Web Storage/5.1: JavaScript: DOM e Seletores/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 05: JavaScript: DOM, Eventos e Web Storage/5.1: JavaScript: DOM e Seletores/script.js
@@ -1,19 +1,20 @@
+// 1:
 function textChange () {
   let parag = document.getElementsByTagName('p');
   parag[0].innerText = 'Estarei em um bom emprego';
 }
 textChange();
 
-// 2:
-function changeYellowToGreen () {
-  let changeColor1 = document.getElementsByClassName('main-content');
-  changeColor1[0].style.backgroundColor = 'rgb(76,164,109)';
-}
-changeYellowToGreen();
+// // 2:
+// function changeYellowToGreen () {
+//   let changeColor1 = document.getElementsByClassName('main-content');
+//   changeColor1[0].style.backgroundColor = 'rgb(76,164,109)';
+// }
+// changeYellowToGreen();
 
-  // 3:
-  function changeRedToWhite () {
-    let changeColor2 = document.getElementsByClassName('center-content');
-    changeColor2[0].style.backgroundColor = 'white';
-  }
-  changeRedToWhite();
+//   // 3:
+//   function changeRedToWhite () {
+//     let changeColor2 = document.getElementsByClassName('center-content');
+//     changeColor2[0].style.backgroundColor = 'white';
+//   }
+//   changeRedToWhite();
