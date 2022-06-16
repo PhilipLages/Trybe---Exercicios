260d2f293477a81930430f86fb2648f87dd2d6aa
Philip Lages <psilvaelages@gmail.com>
2022-06-16 14:50:32 -0300

    Add Part 3 of day 7.2 exercises

--- Web Development Fundamentals/Block 7: Introduction to JavaScript ES6 & Unit Tests/7-2: JavaScript ES6 - Exception flow and Objects/objects.js
+++ Web Development Fundamentals/Block 7: Introduction to JavaScript ES6 & Unit Tests/7-2: JavaScript ES6 - Exception flow and Objects/objects.js
@@ -1,3 +1,4 @@
+// Part 2:
 const order = {
   name: 'Rafael Andrade',
   phoneNumber: '11-98763-1416',
@@ -64,5 +65,69 @@ const orderModifier = (order) => {
 
 orderModifier(order);
 
+// Part 3:
 
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
 
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Add key and values to an object:
+const addNightShift = (object, key, value) => {
+  object[key] = value;
+}
+
+addNightShift(lesson2, 'turno', 'noite');
+
+console.log(lesson2);
+
+// List an object keys:
+const objKeys = obj => Object.keys(obj);
+
+console.log(objKeys(lesson3));
+
+// Show an object length:
+const objLength = obj => Object.keys(obj).length;
+
+console.log(objLength(lesson3));
+
+// Show object values:
+const objValues = obj => Object.values(obj);
+
+console.log(objValues(lesson2));
+
+// Using Object.assign:
+const allLessons = {};
+Object.assign(allLessons, {lesson1, lesson2, lesson3});
+console.log(allLessons);
+
+// Return total number of students:
+const getNumberOfStudents = (obj) => {
+  let total = 0;
+  const array = Object.keys(obj);
+  console.log(array);
+  for (index in array) {
+    total += obj[array[index]].numeroEstudantes;    
+  }
+  return total;
+};
+console.log(getNumberOfStudents(allLessons));
+
+// Get key value based on its position in an object:
+const getValueByPosition = (obj,index) => Object.values(obj)[index];
+console.log(getValueByPosition(lesson2, 2));
