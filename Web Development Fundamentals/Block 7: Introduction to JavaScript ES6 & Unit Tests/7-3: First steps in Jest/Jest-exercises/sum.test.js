35714352b047f57cb610d13c2e1a5da65ab5f06f
Philip Lages <psilvaelages@gmail.com>
2022-06-20 13:25:35 -0300

    Alter sum test

--- Web Development Fundamentals/Block 7: Introduction to JavaScript ES6 & Unit Tests/7-3: First steps in Jest/Jest-exercises/sum.test.js
+++ Web Development Fundamentals/Block 7: Introduction to JavaScript ES6 & Unit Tests/7-3: First steps in Jest/Jest-exercises/sum.test.js
@@ -1,12 +1,11 @@
 const sum = require('./sum.js');
 
 describe('The fuction sum', () => {
-  test('should return sum result', () => {
+  it('should return the result', () => {
     expect(sum(4, 5)).toBe(9);
     expect(sum(0, 0)).toBe(0);
   });
-  test('should return an error if a parameter equals a string', () => {
-    expect(() => sum(4, '5')).toThrowError();
+  it('should return an error if a parameter equals a string', () => {
     expect(() => sum(4, '5')).toThrowError('parameters must be numbers');
   })
 })
