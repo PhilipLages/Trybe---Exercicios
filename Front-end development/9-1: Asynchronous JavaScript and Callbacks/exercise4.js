92f36d8ea9cfd7d44b7413cb4e577cd2d6f5552c
Philip Lages <psilvaelages@gmail.com>
2022-07-06 18:37:33 -0300

    Add exercises 2 to 4

---
+++ Front-end development/9-1: Asynchronous JavaScript and Callbacks/exercise4.js
@@ -0,0 +1,16 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// create the functions sendMarsTemperature below:
+
+const sendMarsTemperature = (callback) => {
+  setTimeout(() => {
+    console.log(`Mars temperature is: ${callback} degrees Celsius`);
+  }, messageDelay()) ;
+}
+
+sendMarsTemperature(getMarsTemperature()); // print "Mars temperature is: 20 degree Celsius", por exemplo
