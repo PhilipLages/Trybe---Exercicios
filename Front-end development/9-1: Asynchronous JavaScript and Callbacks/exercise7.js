2a312b3769919a108952d028fe35eefd640f7d5f
Philip Lages <psilvaelages@gmail.com>
2022-07-06 19:25:30 -0300

    Add exercise 7, needs testing

---
+++ Front-end development/9-1: Asynchronous JavaScript and Callbacks/exercise7.js
@@ -0,0 +1,7 @@
+// Write a test that checks the callback of an uppercase function, which turns the letters of a word into uppercase letters. Remember to beware of false-positives in asynchronous tests.
+
+const uppercase = (str, callback) => {
+  setTimeout(() => {
+    callback(str.toUpperCase());
+  }, 500);
+};
