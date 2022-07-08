af7e42cc31ad81af28c8001cd1006c5f9fe14569
Philip Lages <psilvaelages@gmail.com>
2022-07-08 18:56:43 -0300

    Refactor exercise 2

--- Front-end development/Block 9: JavaScript and asynchronous testing/9.2-Asynchronous JavaScript - Fetch API and async/exercise2/crypto.js
+++ Front-end development/Block 9: JavaScript and asynchronous testing/9.2-Asynchronous JavaScript - Fetch API and async/exercise2/crypto.js
@@ -1,10 +1,24 @@
+const createNewTag = (parent, tag, attribute, value) => {         
+  const createTag = document.createElement(tag);
+  attribute ? createTag.setAttribute(attribute, value) : undefined;
+  parent.appendChild(createTag);  
+}
+
 const createList = (data) => {
   const container = document.querySelector('.container');
-  const list = '<ul>' + data.reduce((html, crypto) => {    
-    html += `<li>${crypto.id} (${crypto.symbol}): US$ ${parseFloat(crypto.priceUsd).toFixed(2)};</li>`;
-    return html;
-  }, ' ') + '</ul>';
-  container.innerHTML = list;
+  createNewTag(container, 'ul', 'id', 'list')
+  const list = document.querySelector('#list');
+  data.forEach((crypto) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `${crypto.id} (${crypto.symbol}): ${parseFloat(crypto.priceUsd).toFixed(2)};`;
+    list.appendChild(listItem);
+  });
+  
+  // const list = '<ul>' + data.reduce((html, crypto) => {    
+  //   html += `<li>${crypto.id} (${crypto.symbol}): US$ ${parseFloat(crypto.priceUsd).toFixed(2)};</li>`;
+  //   return html;
+  // }, ' ') + '</ul>';
+  // container.innerHTML = list;
 }
 
 const getCurrencies = async () => {
